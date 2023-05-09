3702aac57b8c62987b1ff11a1b676182c8595a93
Eric Cheung <nelsonct2222@gmail.com>
2023-05-09 21:55:47 +0100

    Support for Who To Follow

--- content.js
+++ content.js
@@ -1,17 +1,34 @@
 function removePromotedTweets() {
-    const tweetDivs = document.querySelectorAll('div[data-testid="cellInnerDiv"]');
-  
-    tweetDivs.forEach((tweetDiv) => {
-      const promoted = Array.from(tweetDiv.getElementsByTagName('span')).some((span) => {
-        return span.textContent === "Promoted";
-      });
-  
-      if (promoted) {
-        tweetDiv.style.display = 'none';
-      }
+  const tweetDivs = document.querySelectorAll('div[data-testid="cellInnerDiv"]');
+
+  tweetDivs.forEach((tweetDiv) => {
+    const promoted = Array.from(tweetDiv.getElementsByTagName('span')).some((span) => {
+      return span.textContent === "Promoted";
     });
-  }
-  
-  // Run the function initially and set an interval to keep checking for new promoted tweets
-  removePromotedTweets();
-  setInterval(removePromotedTweets, 1000);
+
+    if (promoted) {
+      tweetDiv.style.display = 'none';
+    }
+  });
+}
+
+function removePromotedWhoToFollow() {
+  const whoToFollowDivs = document.querySelectorAll('div[data-testid="UserCell"]');
+
+  whoToFollowDivs.forEach((whoToFollowDiv) => {
+    const promoted = Array.from(whoToFollowDiv.getElementsByTagName('span')).some((span) => {
+      return span.textContent === "Promoted";
+    });
+
+    if (promoted) {
+      whoToFollowDiv.style.display = 'none';
+    }
+  });
+}
+
+// Run the functions initially and set intervals to keep checking for new promoted content
+removePromotedTweets();
+setInterval(removePromotedTweets, 1000);
+
+removePromotedWhoToFollow();
+setInterval(removePromotedWhoToFollow, 1000);
